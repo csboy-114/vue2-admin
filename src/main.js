9f9212656edd25dca0d87e0e983f1b92b9ab1d18
csboy-114 <1060437021@qq.com>
2021-12-25 22:05:56 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-Vue.config.productionTip = false
+import "@/plugins/element-ui.js"
+import axios from 'axios'
+import "@/assets/css/global.css";
+import "@/assets/fonts/iconfont.css"
+
+axios.defaults.baseURL='http://127.0.0.1:8888/api/private/v1'
+Vue.config.productionTip = false;
+Vue.prototype.$http=axios
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
---
+++ src/plugins/element-ui.js
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+import { Button, Form, FormItem, Input,Message } from "element-ui";
+Vue.use(Button).use(Form).use(FormItem).use(Input)
+Vue.prototype.$message=Message
